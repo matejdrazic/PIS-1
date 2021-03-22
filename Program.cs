5904e8354db11ed3de58b9a0043d1fb6dfbddfa1
idagelic <dagelic.ivan@gmail.com>
2021-03-22 18:40:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Novo");
+            Console.WriteLine("Novije");
         }
     }
 }
+/* Dodan console write line novije. */
